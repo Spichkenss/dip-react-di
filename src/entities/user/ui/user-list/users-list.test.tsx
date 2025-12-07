7042735f86ded3b56af692aeb06c853da0422e2a
Spichkenss <spichkens.2002@mail.ru>
2025-12-07 22:29:56 +0300

    update users list tests

--- src/entities/user/ui/user-list/users-list.test.tsx
+++ src/entities/user/ui/user-list/users-list.test.tsx
@@ -1,21 +1,25 @@
 import { Container } from "inversify";
 import { INJECTION_TOKENS } from "../../../../shared/lib/di/tokens";
-import type { IUserRepository } from "../../model/user.types";
 import { UserMockApi } from "../../model/api/user-api.mock";
 import { render, screen, waitFor } from "@testing-library/react";
 import { DiProvider } from "../../../../shared/lib/di";
 import { UsersListEntry } from "./users-list.entry";
+import { createContainer } from "../../../../shared/lib/di/create-container";
+import { createContainerModule } from "../../../../shared/lib/di/create-container-module";
 
 describe("UsersListEntry", () => {
   let container: Container;
 
-  beforeEach(() => {
-    container = new Container();
-
-    container
-      .bind<IUserRepository>(INJECTION_TOKENS.USER_API)
-      .to(UserMockApi)
-      .inSingletonScope();
+  beforeAll(() => {
+    const userModule = createContainerModule((options) => {
+      options
+        .bind(INJECTION_TOKENS.USER_API)
+        .to(UserMockApi)
+        .inSingletonScope();
+    });
+    container = createContainer((container) => {
+      container.load(userModule);
+    });
   });
 
   test("renders users from UserMockApi", async () => {
