e280430328e62f0d2944fd74a00d7cbcfa9b85ac
Spichkenss <spichkens.2002@mail.ru>
2025-12-08 00:06:44 +0300

    .

--- src/entities/user/model/api/user-api.prod.ts
+++ src/entities/user/model/api/user-api.prod.ts
@@ -1,8 +1,10 @@
 import type { IUserRepository, User } from "../user.types";
 
 export class UserProdApi implements IUserRepository {
+  private readonly API_URL = "https://jsonplaceholder.typicode.com";
+
   async getUsers(): Promise<User[]> {
-    const result = await fetch("https://jsonplaceholder.typicode.com/users");
+    const result = await fetch(`${this.API_URL}/users`);
     return result.json();
   }
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { DiProvider } from "./shared/lib/di";
 import { rootContainer } from "./app/modules/root.module.ts";
-import App from "./app/App.tsx";
 import "./index.css";
+import App from "./app/app.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
